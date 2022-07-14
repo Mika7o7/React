aead26d264af380c13a377beae2c29fc6e3a9e07
Mika7o7 <mikaohanyan881@gmail.com>
2022-07-14 03:36:59 +0000

    creat and delet post

---
+++ src/components/PostList.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import PostItem from "./PostItem";
+
+
+const PostList = ({posts, remove}) => {
+    if(!posts.length) {
+        return(
+            <h1 style={{textAlign: 'center'}}>
+                Posts not faund
+            </h1>
+        )
+    }
+
+    return (
+        <div>
+            <h1 style={{textAlign: 'center'}}>
+                list of posts
+            </h1>
+            {posts.map((post, index) =>
+            <PostItem remove={remove} number={index + 1} post={post} key={post.id}/>
+            )}
+        </div>
+    )
+};
+
+export default PostList;
